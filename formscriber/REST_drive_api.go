a9ca2c090dd15171ca4f5562098478c816b5404b
defigo <geoffrey.a.dean@gmail.com>
2021-03-19 07:41:51 -0400

    CSSS updates

--- formscriber/REST_drive_api.go
+++ formscriber/REST_drive_api.go
@@ -145,14 +145,109 @@ func driveAuth(h http.HandlerFunc) http.HandlerFunc {
 				fmt.Println("\nGet Drive Files complete!\n")
 				return
 			}
-		case "POST":
-			fmt.Println("\nPOST FOUND ERROR\n")
-			fmt.Fprintf(w, "Sorry, only GET methods are supported.")
-			return
 
+			//POST API
+		case "POST":
+
+			fmt.Println("\nCheck POST method\n")
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Println("\nCould not parse POST\n")
+				fmt.Println(err)
+				return
+			}
+			fmt.Println("\n POST was parsed\n")
+			//
+
+			fmt.Println("\nparsing POST DRIVE body!\n")
+			fmt.Println(string(body))
+			// parse out the intent/action
+			if len(string(body)) < 1 {
+				fmt.Println("Body empty! Default response")
+				response := `{
+					"kind": "drive#fileList",
+					"incompleteSearch": false,
+					"files": [
+					 {
+					  "kind": "drive#file",
+					  "id": "1F_0unyG-HHrOqRj1o-QfEQDxmkl_xw5xSo4dVLyqvPA",
+					  "name": "Test Account",
+					  "mimeType": "application/vnd.google-apps.document"
+					 }
+					]
+				   }`
+				fmt.Fprintf(w, response)
+
+				fmt.Println("\n RESPONSE BACK WEB APP: " + response)
+				fmt.Fprintf(w, response)
+			} else {
+
+				driveUrl := gjson.Get(string(body), "driveUrl")
+				fmt.Println("\nDrtive URL parsed\n")
+				//drive.google.com/drive/u/0/folders/([a-zA-Z0-9-_]+)
+				regex, _ := regexp.Compile("/folders/([a-zA-Z0-9-_]+)")
+				fmt.Println("\nRegex step1\n")
+				//fmt.Println(r.FindString(url.String()))
+				driveid := regex.FindString(driveUrl.String())
+				fmt.Println("\nRegex find url\n")
+
+				// Extract Drive ID:
+
+				fmt.Println("\nDriveID without trim: " + driveid)
+				fmt.Println("\nDriveID with trim: " + trimLeftChars(driveid, 9))
+
+				driveid = trimLeftChars(driveid, 9)
+				fmt.Println("\nDriveID Final: " + driveid)
+
+				url := "https://www.googleapis.com/drive/v3/files?q='" + driveid + "'%20in%20parents"
+
+				fmt.Println("\nDRIVE URL: " + url)
+
+				token := getAccessToken()
+				fmt.Println("\nTOKEN: " + token)
+
+				//Initiate Google Drive Call
+				method := "GET"
+				fmt.Println("\nCall Google step1\n")
+				payload := strings.NewReader(``)
+
+				client := &http.Client{}
+				req, err := http.NewRequest(method, url, payload)
+				fmt.Println("\nCall Google step2\n")
+				if err != nil {
+					fmt.Println("\nError calling google API1\n")
+					fmt.Println(err)
+					return
+				}
+				fmt.Println("\nGoogel APi called1\n")
+				req.Header.Add("Authorization", "Bearer "+token)
+				req.Header.Add("Content-Type", "application/json")
+				fmt.Println("\nGoogel APi called2\n")
+				res, err := client.Do(req)
+				if err != nil {
+					fmt.Println("\nError calling google API2\n")
+					fmt.Println(err)
+					return
+				}
+				defer res.Body.Close()
+				fmt.Println("\nReturn Response back to client1\n")
+				body, err = ioutil.ReadAll(res.Body)
+				if err != nil {
+					fmt.Println("\nERROR Return Response back to client1\n")
+					fmt.Println(err)
+					return
+				}
+				fmt.Println(string(body))
+				fmt.Fprintf(w, string(body))
+				fmt.Println("\nGet Drive Files complete!\n")
+				return
+
+				fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+				return
+			}
 		default:
 			fmt.Println("\nUnhandled ERROR!\n")
-			fmt.Fprintf(w, "Sorry, only GET methods are supported.")
+			fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 			return
 		}
 		fmt.Println("\nProcess done1\n")
--- formscriber/go.mod
+++ formscriber/go.mod
@@ -3,5 +3,6 @@ module hello
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/tidwall/gjson v1.6.8 // indirect
 )
